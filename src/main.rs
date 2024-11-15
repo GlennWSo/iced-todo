f61b8d4ad43dba4d6a0a3abbcc530a75b2b0670e
gws <glennws@pm.me>
2024-11-15 14:36:46 +0100

    init todo

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,83 @@
+use std::collections::HashMap;
+
+use derivative::Derivative;
 use iced::{
+    // advanced::graphics::core::Element,
+    advanced::Widget,
     border::width,
-    widget::{button, column, container, text},
+    futures::FutureExt,
+    widget::{button, column, container, row, text, text_input},
     Element,
     Length::Fill,
     Theme,
 };
+use uuid::Uuid;
 
-#[derive(Default)]
-struct Counter {
-    value: i32,
+// #[derivative(Hash)]
+// #[derive(Derivative)]
+#[derive(Hash, Debug)]
+struct Task {
+    description: String,
+    id: Uuid,
 }
 
-#[derive(Clone, Debug, Copy)]
-enum Message {
-    Increment,
-    Decrement,
-}
-
-impl Counter {
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => self.value += 1,
-            Message::Decrement => self.value -= 1,
+impl Task {
+    fn new(description: String) -> Self {
+        Self {
+            description,
+            id: Uuid::new_v4(),
         }
     }
-    fn theme(&self) -> Theme {
-        Theme::Nightfly
-    }
+}
+
+#[derive(Default)]
+struct State {
+    input_value: String,
+    todo: HashMap<Uuid, Task>,
+    finnished: Vec<Task>,
+}
+
+#[derive(Clone, Debug)]
+enum Message {
+    Input(String),
+    CreateTask,
+    TaskCompleted(Uuid),
+    TaskRedo(usize),
+    TodoDeleted(usize),
+    FinnishedDeleted(usize),
+}
+
+impl State {
     fn view(&self) -> Element<Message> {
-        container(
-            column![
-                button(text("+").center().width(Fill)).on_press(Message::Increment),
-                // .height(Fill),
-                text(self.value).center().width(Fill),
-                button(text("-").center().width(Fill))
-                    .on_press(Message::Decrement)
-                    .clip(true)
-            ]
-            .spacing(11)
-            .width(100),
-        )
-        .center_x(Fill)
-        .center_y(Fill)
-        .into()
+        let new_todo = row![
+            button("add todo").on_press(Message::CreateTask),
+            text_input("Do this", &self.input_value).on_input(|input| Message::Input(input))
+        ];
+        let todos = column(self.todo.values().map(|task| {
+            let tx: Element<Message> = button(task.description.as_str())
+                .on_press(Message::TaskCompleted(task.id))
+                .into();
+            tx
+        }));
+        let main_box = column![new_todo, todos,];
+        main_box.into()
+    }
+    fn update(&mut self, m: Message) {
+        match m {
+            Message::Input(txt) => self.input_value = txt,
+            Message::CreateTask => {
+                // self.todo.push(Task::new(self.input_value.clone()));
+                let task = Task::new(self.input_value.clone());
+                self.todo.insert(task.id, task);
+            }
+            Message::TaskCompleted(id) => todo!(),
+            Message::TaskRedo(_) => todo!(),
+            Message::TodoDeleted(_) => todo!(),
+            Message::FinnishedDeleted(_) => todo!(),
+        }
     }
 }
 
-#[test]
-fn it_counts_properly() {
-    let mut counter = Counter { value: 0 };
-
-    counter.update(Message::Increment);
-    counter.update(Message::Increment);
-    counter.update(Message::Decrement);
-
-    assert_eq!(counter.value, 1);
-}
-
-fn main() -> iced::Result {
-    iced::application("Lets count", Counter::update, Counter::view)
-        .theme(Counter::theme)
-        .run()
+fn main() -> std::result::Result<(), iced::Error> {
+    iced::run("Todo", State::update, State::view)
 }
